c6a1a05b780f7f6533aaf5ed3f0ed142ab72f272
jenmlucas <jenmlucas1@gmail.com>
2021-11-01 16:10:40 -0700

    created html file to generate webpage

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github]= profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw err; 
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
