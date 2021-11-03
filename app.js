a32b426dd4b3cf84ab2d629fdbf1bd2fa220a0a6
jenmlucas <jenmlucas1@gmail.com>
2021-11-03 14:53:40 -0700

    updated html template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -131,18 +133,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-    })
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+        const pageHTML = generatePage(portfolioData);
 
-// const pageHTML = generatePage(name, github);
+        fs.writeFile("./index.html", pageHTML, err => {
+            if (err) throw new Error(err);
 
-// fs.writeFile("./index.html", pageHTML, err => {
-//     if (err) throw err; 
-
-//     console.log("Portfolio complete! Check out index.html to see the output!");
-// });
-
-
-/////// on 9.3.5
+            console.log("Page created! Check out index.html in this directory to see it!");
+        });
+    });
