16ee56227a359fe39160b18a9db9fd8e2d3cdc7c
jenmlucas <jenmlucas1@gmail.com>
2021-11-03 11:07:29 -0700

    added an about section

--- app.js
+++ app.js
@@ -29,9 +29,22 @@ const promptUser = () => {
             }
         },
         {
-            type: "input",
-            name: "about",
-            message: "Provide some information about yourself:"
+            type: "confirm",
+            name: "confirmAbout",
+            message: "Would you like to enter some information for an 'About' section?",
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
